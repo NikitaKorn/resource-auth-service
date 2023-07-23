a2db2c9f12abc665b6a70c880b8ca6ccf62d7b06
Kornev.NE <Kornev.NE>
2023-07-23 16:01:30 +0700

    init commit

---
+++ src/main/java/com/example/resourceauthserver/ResourceAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.resourceauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resourceauthserver/ResourceAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourceauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
